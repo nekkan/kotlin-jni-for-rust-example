e715822049f55a6f78b084b59341c62fc79665bb
nekkan <nxkkdev@gmail.com>
2020-11-16 21:31:40 -0300

    Update build.gradle.kts.

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,3 +9,8 @@ repositories {
 dependencies {
     implementation(kotlin("stdlib"))
 }
+
+println(
+    "[WARN] If you want to compile all or update the DLL/SO/DYLIB Rust code, " +
+            "run the run_all.sh file at scripts folder."
+)
